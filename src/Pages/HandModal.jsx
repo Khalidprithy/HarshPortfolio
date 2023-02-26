767c036f8653725bfe80c465a47bee111fca0b51
Khalidprithy <khalidprithy@gmail.com>
2023-02-26 13:28:36 +0600

    Blur added

--- src/Pages/HandModal.jsx
+++ src/Pages/HandModal.jsx
@@ -5,7 +5,7 @@ const HandModal = ({ setOpenModalHand }) => {
     return (
         <div
             // onClick={() => setOpenModal(false)}
-            className='backdrop-blur fixed inset-0 z-50 overflow-hidden'>
+            className='backdrop-blur-lg fixed inset-0 z-50 overflow-hidden'>
             {/* <div className='w-screen h-screen backdrop-blur z-0'></div> */}
             <section className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
                 <img
